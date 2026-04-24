0c873db5917d213b5f942e363484722250a97f5f
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-04-24 13:21:24 +0100

    Add Profile Page

---
+++ web/src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div className="profile-page">
+      <h1>Your Profile</h1>
+      {/* Profile form to be implemented */}
+    </div>
+  );
+}
+
+export default Profile;
