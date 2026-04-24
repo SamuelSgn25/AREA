bdaa8d986df684eaed131648f7f5757dd325d660
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-04-24 13:22:20 +0100

    Add workflows for  web app configuration

---
+++ web/src/pages/Workflows.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Workflows() {
+  return (
+    <div className="workflows-page">
+      <h1>My Workflows</h1>
+      <p>Create and manage your AREA workflows</p>
+      {/* Workflows list to be implemented */}
+    </div>
+  );
+}
+
+export default Workflows;
