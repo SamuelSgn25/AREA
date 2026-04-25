de85625c8799d19e7ad10f0e32f4708c94053aa9
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-04-25 21:09:19 +0100

    For defining user model interface

---
+++ server/dist/models/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// ==========================================
+// User Model Interface
+// ==========================================
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=index.js.map
